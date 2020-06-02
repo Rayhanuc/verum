1c044976dc910f304b7cdf858165f3c91b1c8f0d
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-06-02 23:45:45 +0600

    4.35 - Showing featured posts in the search section using the Customizer and Kirki Repeater fields (challenging)
    4.35 - Showing featured posts in the search section using the Customizer and Kirki Repeater fields (challenging)

--- wp-content/themes/verum/customizer/main-config.php
+++ wp-content/themes/verum/customizer/main-config.php
@@ -31,4 +31,67 @@ if(class_exists('Kirki')) {
 			'right' => esc_html__( 'Right Sidebar', 'verum' ),
 		],
 	] );
+
+	Kirki::add_section( 'search_settings', array(
+	    'title'          => esc_html__( 'Search Settings', 'verum' ),
+	    'panel'          => 'verum_panel',
+	    'priority'       => 160,
+	) );
+
+	Kirki::add_field( 'verum_options', [
+		'type'        => 'text',
+		'settings'    => 'search_heading',
+		'label'       => esc_html__( 'Heading', 'verum' ),
+		'section'     => 'search_settings',
+		'default'     => __('Latest Posts','verum'),
+		'priority'    => 10,
+	] );
+
+	Kirki::add_field( 'verum_options', [
+		'type'        => 'select',
+		'settings'    => 'search_post_source',
+		'label'       => esc_html__( 'Post Source', 'verum' ),
+		'section'     => 'search_settings',
+		'default'     => 'latest',
+		'placeholder' => esc_html__( 'Select an option...', 'verum' ),
+		'priority'    => 10,
+		'multiple'    => 1,
+		'choices'     => [
+			'latest' => esc_html__( 'Latest Posts', 'verum' ),
+			'featured' => esc_html__( 'Featured Posts', 'verum' ),
+		],
+	] );
+
+	Kirki::add_field( 'verum_options', [
+		'type'        => 'repeater',
+		'label'       => esc_html__( 'Featured Posts', 'verum' ),
+		'section'     => 'search_settings',
+		'priority'    => 10,
+		'row_label' => [
+			'type'  => 'field',
+			'value' => esc_html__( 'Featured Post', 'verum' ),
+			'field' => 'post'
+		],
+		'button_label' => esc_html__('Add new', 'verum' ),
+		'settings'     => 'search_featured_post',
+		'fields' => [
+			'post'  => [
+				'type'        => 'select',
+				'label'       => esc_html__( 'Featured Post', 'verum' ),
+				'choices' => Kirki_Helper::get_posts(array(
+					'posts_per_page' => -1,
+					'post_status' => 'publish',
+					'post_type' => 'post'
+				))
+			],
+
+		],
+		'active_callback' => array(
+			array(
+				'setting' => 'search_post_source',
+				'value' => 'featured',
+				'operator' => '=='
+			)
+		),
+	] );
 }
--- wp-content/themes/verum/templates/search/overlay.php
+++ wp-content/themes/verum/templates/search/overlay.php
@@ -1,4 +1,9 @@
 <!--search overlay start-->
+<?php
+ $verum_post_source = get_theme_mod("search_post_source","latest");
+ $verum_search_heading = get_theme_mod("search_heading",__("Latest Posts","verum"));
+
+?>
 <div class="search-wrap">
     <div class="overlay">
         <?php get_search_form(  ); ?>
@@ -8,7 +13,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <h4 class="text-light search-blog-title">
-                       <?php _e("Latest Posts","verum"); ?>
+                       <?php echo esc_html($verum_search_heading,'verum'); ?>
                     </h4>
                 </div>
             </div>
@@ -17,6 +22,8 @@
             <div class="container">
                 <div class="row">
                     <?php
+
+                    if("latest"==$verum_post_source) :
                     $verum_latest_posts_args = array(
                         'posts_per_page' => 4,
                         'post_status' => 'publish',
@@ -65,6 +72,63 @@
                     <?php
                     endwhile;
                     wp_reset_query();
+                else:
+                    // Featured posts section
+
+                    $_verum_featured_posts_id = get_theme_mod("search_featured_post");
+                    $verum_featured_posts_id = array_column($_verum_featured_posts_id,'post');
+                    // print_r($verum_featured_posts);
+                    $verum_featured_posts_args = array(
+                        'post_status' => 'publish',
+                        'post_type' => 'post',
+                        'ignore_sticky_post' => 1,
+                        'post__in' => $verum_featured_posts_id,
+                        'orderby' => 'post__in'
+                    );
+
+                    $verum_featured_posts = new WP_Query($verum_featured_posts_args);
+
+                    while($verum_featured_posts->have_posts()) :
+                        $verum_featured_posts->the_post();                        
+                    ?>
+
+                    <div class="col-md-3">
+                        <article class="post">
+                            <div class="post-img">
+                                <?php the_post_thumbnail('medium'); ?>
+                                
+                            </div>
+                            <div class="post-header">
+                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <div class="post-meta">
+                                    <?php
+                                        $verum_categories = get_the_category();
+                                        if($verum_categories):
+                                            echo '<ul class="cat">';
+                                            foreach ($verum_categories as $verum_categorie) :
+                                                printf('<li><a href="%s">%s</a></li>', get_category_link( $verum_categorie ), $verum_categorie->name);
+                                                break;
+                                    ?>
+                                            <?php
+                                                endforeach;
+                                                echo '</ul>';
+                                            ?>
+                                        
+                                        <div class="separation"></div>
+                                    <?php
+                                        endif;
+                                    ?>
+                                    <div class="post-date"><a href="<?php the_permalink(); ?>"><?php echo get_the_date('i F Y'); ?></a></div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                    <?php
+                    endwhile;
+                    wp_reset_query();
+
+                    endif;
                     ?>
                 </div>
             </div>
